a53ae293c050b250d9714acd9665b2e1a3ea3842
Sivav15 <sivanathanv1999@gmail.com>
2024-07-22 10:18:24 +0530

    add folder structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        // {
+        //   path: "/",
+        //   element: <Navigate to="/task" replace />,
+        // },
+        {
+          path: "task",
+          element:
+            <AuthProtectedRoute>
+              <Task />
+            </AuthProtectedRoute>
+          ,
+        },
+        {
+          path: "/",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        // {
+        //   path: "*",
+        //   element: <NotFound />,
+        // },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
